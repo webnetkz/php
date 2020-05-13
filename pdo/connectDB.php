2dbaed55e2a215d31dbc599f9f2aa69f7d90a048
webnetkz <8gg001@gmail.com>
2020-05-13 11:43:32 +0600

    append connect db and first pdo

---
+++ pdo/connectDB.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+    // Данные для подключения к базе данных
+    $driver = 'mysql';
+    $host = 'localhost';
+    $db_name = 'dwe';
+    $db_user = 'root';
+    $db_pass = '';
+    $charset = 'utf8';
+    $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+    try{
+        // Создание обьекта PDO и передача данных
+        // для подключения в конструктор класса
+        $pdo = new PDO(
+                        "$driver:host=$host;
+                        dbname=$db_name;
+                        charset=$charset",
+                        $db_user,
+                        $db_pass,
+                        $options
+                    );
+    // Отладка ошибок подключения
+    }catch(PDOException $e) {
+        die('Ошибка подключения к базе данных');
+    }
