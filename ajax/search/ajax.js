f8a1120de5e5f59bfc7979ba45a51ae563efdc11
webnetkz <8gg001@gmail.com>
2020-05-18 12:23:56 +0600

    append autocomplete js

---
+++ ajax/search/ajax.js
@@ -0,0 +1,25 @@
+﻿document.querySelector('#inp').oninput = function() {
+
+        inp = this.value.trim(); // Значение с поля ввода
+
+        console.log(inp);
+
+        let countrys;
+        if(typeof countrys !== 1) {
+            let userData = document.forms.myForm.inp.value;
+    
+            userData = encodeURIComponent(userData);
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'search.php');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === 4 && xhr.status === 200) {
+                    countrys = xhr.responseText;
+                    let res = document.querySelector('#res').innerHTML = countrys;
+                }
+            }
+
+            xhr.send('inp=' + userData);
+        
+}
---
+++ ajax/search/search.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+    require_once '../../pdo/connectDB.php';
+
+    $sql = "SELECT name_ru FROM net_country";
+    $res = $pdo->query($sql);
+
+    $res = $res->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($res as $k => $v) {
+        foreach($v as $k => $v) {
+            $countrys .= '<li>'.$v.'</li>';
+        }
+    }
+
+    $data = $countrys;
+
+
+   print_r($data);
